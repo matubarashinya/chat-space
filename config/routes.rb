aea05b511a8491db5a8e31bb60caf52daf0531a9
matubarashinya <is2baseball1130@icloud.com>
2019-11-20 18:20:15 +0900

    自動更新機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
    resources :messages, only: [:index, :create]
+   namespace :api do
+    resources :messages, only: :index, defaults: { format: 'json' }
+    # namespace :ディレクトリ名 do ~ endと囲む形でルーティングを記述すると、そのディレクトリ内のコントローラのアクションを指定できます。
+    # defaultsオプションを利用して、このルーティングが来たらjson形式でレスポンスするよう指定しています。
+  end
   end
 end
